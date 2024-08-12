a1c1466a146900fd737375ee2574745a227a3a2f
Kent Martin Åsvang <github.com@asvang.no>
2024-08-12 19:21:42 +0200

    basic app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,37 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+    <h1>Formålserklæring</h1>
+    <p>Hold det jeg lover.</p>
+    <p>Gjør det jeg sier jeg skal gjøre, for meg selv og for andre, til riktig tid</p>
+    <p>Ikke utsett til i morgen det jeg kan gjøre i dag</p>
+    <p>Planlegg for morgendagen idag</p>
+    <p>Ha tid til andre enn mine egne gjøremål</p>
+    <p>Still opp for andre og for meg selv.</p>
+    <p>Ikke lyve, for meg selv eller andre.</p>
+    <p>Gjør mer enn det som forventes av meg.</p>
+    <p>Blod er ikke familie</p>
+    <p>Det er bedre å slappe av enn å kaste bort tid.</p>
+</section>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+<section>
+    <h1>Mission Statement</h1>
+    <p>Keep what I promise.</p>
+    <p>Do what I say I will do, for myself and for others, at the right time.</p>
+    <p>Do not put off until tomorrow what I can do today.</p>
+    <p>Plan for tomorrow today.</p>
+    <p>Have time for others besides my own tasks.</p>
+    <p>Show up for others and for myself.</p>
+    <p>Do not lie, to myself or others.</p>
+    <p>Do more than is expected of me.</p>
+    <p>Blood is not family.</p>
+    <p>It is better to relax than to waste time.</p>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
