3ea12d6900173316b3465959658f767c1d01ff19
Kent Martin Åsvang <github.com@asvang.no>
2024-10-13 18:47:51 +0200

    populated post_statuses, draft published and archived

---
+++ data/migrations/2024-10-13T18:39:53.832494_insert_post_statuses/down.sql
@@ -0,0 +1,4 @@
+
+DELETE FROM post_status WHERE id = 0;
+DELETE FROM post_status WHERE id = 1;
+DELETE FROM post_status WHERE id = 2;
---
+++ data/migrations/2024-10-13T18:39:53.832494_insert_post_statuses/up.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO post_status (id, name, description)
+VALUES (0, 'Draft', 'The post is a draft and not visible to the public'),
+       (1, 'Published', 'The post is published and visible to the public'),
+       (2, 'Archived', 'The post is archived and not visible to the public');
